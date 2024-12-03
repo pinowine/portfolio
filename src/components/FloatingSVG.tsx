f1a24b5bef6a12b8b09c0c6dbd080cba2fb4bacf
pinowine <chenzeyuan36@gmail.com>
2024-12-03 13:56:53 +0800

    WIP 5.1

--- src/components/FloatingSVG.tsx
+++ src/components/FloatingSVG.tsx
@@ -1,20 +1,24 @@
 ﻿import { useRive } from "@rive-app/react-canvas";
 import Skeleton from "./Skeleton";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 interface FloatingSVGProps {
   suffix: string;
 }
 
 const FloatingSVG: React.FC<FloatingSVGProps> = ({ suffix }) => {
+  const [isRiveLoaded, setIsRiveLoaded] = useState(false);
   const { RiveComponent } = useRive({
     src: `${process.env.NODE_ENV === "production" ? "/" : "/"}rive/portfolio-${suffix}.riv`,
     stateMachines: ["State Machine 1"],
     autoplay: true,
+    onLoad: () => setIsRiveLoaded(true),
+    isTouchScrollEnabled: true,
   });
 
   return (
     <div className="w-full h-full z-0" id={`rive-home-${suffix}`}>
+      {!isRiveLoaded && <Skeleton type="image" />}
       <Suspense fallback={<Skeleton type="image" />}>
         <RiveComponent />
       </Suspense>
